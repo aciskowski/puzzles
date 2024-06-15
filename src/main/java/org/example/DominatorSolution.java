e3f88ec96741bd1aab78c422f1a75950ebb57663
Artur Ciskowski <aciskowski@unity-t.pl>
2024-06-15 19:54:38 +0200

    Dominators comments

--- src/main/java/org/example/DominatorSolution.java
+++ src/main/java/org/example/DominatorSolution.java
@@ -6,20 +6,20 @@ import java.util.List;
 public class DominatorSolution {
 
     public static void testIfBothBracketsAreSymetricalClose(String text) {
-        List<String> result = new ArrayList<String>();
+        List<String> result = new ArrayList<String>();//typ domyslny, nie trzeba wpisywac String w ArrayList, po za tym lista rezultato nie potrzebna, ma zwracac tylko true jesli CALY string jest poprawny i false jesli nie
         StringBuilder actuallyText = new StringBuilder(text);
-        if (text.length() % 2 > 0) {
-            result.add("false");
+        if (text.length() % 2 > 0) {// ==1 pewnie lepiej, ale sie nei czepiam
+            result.add("false");//return false
         } else {
             while (actuallyText.length() > 0) {
                 if (isMatchingPair(actuallyText.charAt(0), actuallyText.charAt(actuallyText.length() - 1))) {
                     actuallyText.deleteCharAt(0);
                     actuallyText.deleteCharAt(actuallyText.length() - 1);
-                    result.add("true");
+                    result.add("true");//nie potrzebne
                 } else if (!isMatchingPair(actuallyText.charAt(0),
-                        actuallyText.charAt(actuallyText.length() - 1))) {
-                    result.add("false");
-                    break;
+                        actuallyText.charAt(actuallyText.length() - 1))) {//po prostu else
+                    result.add("false");//return false
+                    break;//nie potrzebne
                 }
             }
         }
@@ -35,6 +35,7 @@ public class DominatorSolution {
         else {
             return false;
         }
+        //dla calego bloku powyzej po prostu return i warunek w ifie
     }
 
 }
